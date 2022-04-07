f9aadf4083136cc2613d2230e4d9b310f18122cc
Ron <Rayron5656@Gmail.com>
2022-04-07 18:09:01 +0300

    add two component and router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { DashBoardComponentComponent } from './dash-board-component/dash-board-component.component';
+import { Routes,RouterModule } from '@angular/router'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentComponent,
+    DashBoardComponentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
